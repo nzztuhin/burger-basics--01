d110c1568ff1476f73d390aa5e07829aeb3889c1
nzztuhin <64777785+nzztuhin@users.noreply.github.com>
2022-09-28 10:38:18 +0600

    build controls p1

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from './Burger.css';
 import BugerIngredient from "./Burgeringredient/Burgeringredient";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map(( _, i) => {
             return <BugerIngredient key={igKey + i} type={igKey} /> 
         });
-    }) ;
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []) ;
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
     return(
         <div className={classes.Burger}>
             <BugerIngredient type="bread-top" />
